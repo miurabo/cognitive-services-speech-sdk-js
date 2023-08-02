be8b3c4155c34043bb60bbfa7cf916a17eaa250e
miura <miura@ai-j.jp>
2023-08-02 14:36:06 +0900

    ビルド済みのコードをGit上で公開状態にしたブランチを作る

--- src/sdk/SpeechRecognitionCanceledEventArgs.ts
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT license.
-
-import {
-    CancellationEventArgsBase,
-} from "./CancellationEventArgsBase";
-
-export class SpeechRecognitionCanceledEventArgs extends CancellationEventArgsBase {
-}
